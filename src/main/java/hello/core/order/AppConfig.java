ed9b9afa0c6de225b1775b57b1b7cf6e71d03e8d
chae <stat_chae0@sookmyung.ac.kr>
2022-03-02 15:35:02 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -12,18 +12,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@bean memberService -> new MemoryMemberRepository
+    //@bean orderService -> new MemoryMemberRepository
+    //이러면 싱글톤이 깨지는거 아냐?? -> 확인해본결과 아님
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+    //일줄 알았는데
+
+    //call AppConfig.memberService
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+    //3개만 호출 됨
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
